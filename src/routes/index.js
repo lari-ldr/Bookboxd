b4430c19620771ee39b76bde98ed6f741370225b
lari-ldr <larissa.academico01@gmail.com>
2021-01-22 20:21:34 -0300

    included users routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 // responsável por carregar todas as rotas da aplicação
 import express from 'express';
 import booksRoute from './books';
+import usersRoute from './users';
 
 const router = express.Router();
 
 router.use('/books', booksRoute);
+router.use('/users', usersRoute);
 router.get('/', (req, res)=> res.send('A simple API for book lovers ;)'));
 export default router;
