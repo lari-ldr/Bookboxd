e5e9de1f823ac3d3fc4cc6b83f07ae1982c8dc27
lari-ldr <larissa.academico01@gmail.com>
2021-01-27 13:34:49 -0300

    move other imcompletes tests to new features branch

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,10 @@
 import express from 'express';
 import booksRoute from './books';
 import usersRoute from './users';
-import reviewsRoute from './reviews';
 
 const router = express.Router();
 
 router.use('/books', booksRoute);
-router.use('/books', reviewsRoute);
 router.use('/users', usersRoute);
 router.get('/', (req, res)=> res.send('A simple API for book lovers ;)'));
 export default router;
